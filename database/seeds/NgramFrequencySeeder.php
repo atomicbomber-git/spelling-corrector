64d87f41eb8ced59ead9edba5a0e8e83fdfcda40
James Patrick Keegan <atomicbomber.email@gmail.com>
2021-01-30 19:17:45 +0700

    Updates.

--- database/seeds/NgramFrequencySeeder.php
+++ database/seeds/NgramFrequencySeeder.php
@@ -18,73 +18,24 @@ class NgramFrequencySeeder extends Seeder
      */
     public function run()
     {
-        $lazyCollection = new LazyCollection();
+        $sentence_files_root_dir = database_path("seeds/sentences");
+        $sentence_filenames = scandir($sentence_files_root_dir);
 
-        $filePath = __DIR__ . "/../seeds/data/abstract.txt";
-        $fileHandle = fopen($filePath, "r");
+        foreach ($sentence_filenames as $sentence_file) {
+            if (in_array($sentence_file, [".", ".."])) {
+                continue;
+            }
+
+//            $filepath =
+
+            $file = file_get_contents("{$sentence_files_root_dir}/{$sentence_file}");
 
 
-        $lazySentenceTokenizer = new LazySentenceTokenizer();
 
-        $val =
-            LazyCollection::make(function () {
-                $filePath = __DIR__ . "/../seeds/data/abstract.txt";
-                $fileHandle = fopen($filePath, "r");
 
-                while (($line = fgets($fileHandle)) !== false) {
-                    yield $line;
-                }
-            })
-                ->chunk(10)
-                ->each(function ($chunk) use($lazySentenceTokenizer) {
-                    DB::beginTransaction();
-                    $chunk->map(fn($line) => LazyCollection::make($lazySentenceTokenizer->tokenize($line)))
-                        ->map(function (LazyCollection $sentences) {
-                            return LazyCollection::make(function () use ($sentences) {
-                                foreach ($sentences as $sentence) {
-                                    yield tokenize($sentence);
-                                }
-                            });
-                        })
-                        ->each(function (LazyCollection $tokenizedSentences) {
-                            $tokenizedSentences->each(function ($tokenizedSentence) {
+        }
 
-                                $tokens = array_map(fn($token) => trim($token, "[]().\t\n\r\0\x0B"), $tokenizedSentence);
 
-                                $ngrams = array_map(
-                                    fn($token) => explode(' ', $token),
-                                    Ngram::ngramWithPadding($tokens, N_OF_GRAMS, ' '),
-                                );
 
-                                foreach ($ngrams as $ngram) {
-                                    $filter = [
-                                        "word1" => $ngram[0],
-                                        "word2" => $ngram[1],
-                                        "word3" => $ngram[2],
-                                    ];
-
-                                    $recordCount = DB::table((new NgramFrequency)->getTable())
-                                        ->where($filter)
-                                        ->count();
-
-                                    if ($recordCount == 0) {
-                                        DB::table((new NgramFrequency)->getTable())
-                                            ->insertOrIgnore(array_merge($filter, [
-                                                'created_at' => now(),
-                                                'updated_at' => now(),
-                                            ]));
-                                        continue;
-                                    }
-
-                                    DB::table((new NgramFrequency)->getTable())
-                                        ->where($filter)
-                                        ->increment('frequency');
-                                }
-
-                            });
-                        });
-
-                    DB::commit();
-                });
     }
 }
